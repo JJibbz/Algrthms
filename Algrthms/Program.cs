c8fd09f4b7bb77a6a69de20251d3552d8646c1db
JJibbz <vkazancev322@gmail.com>
2024-02-16 19:45:25 +0700

    Добавьте файлы проекта.

---
+++ Algrthms/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Algrthms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("login", "name", true);
+            User user2 = new User("login1", "name1", false);
+            User user3 = new User("login2", "name2", false);
+            User user4 = new User("login3", "name3", true);
+            User[] users = {user1, user2, user3, user4};
+
+            foreach (User user in users)
+            {
+                Greetings(user.Name);
+                if (user.IsPremium)
+                {
+                    ShowAds();
+                }
+            }
+
+        }
+
+        static void Greetings(string name)
+        {
+            Console.WriteLine($"Здравствуйте, {name}");
+
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Algrthms/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algrthms
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string _login, string _name, bool _isPremium)
+        {
+            Login = _login;
+            Name = _name;
+            IsPremium = _isPremium;
+        }
+    }
+}
